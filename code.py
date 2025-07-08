15c33402b2d5c54a0bc36b2a4c118a613862aa0a
Ashwin Vadde <ashwin.vadde@bilvantis.io>
2025-07-08 14:56:02 +0530

    test

--- code.py
+++ code.py
@@ -4,4 +4,15 @@ secret code = ydgiysygiuzbcusucnc
 
 password =Ashwin@123
 
-secret code = ydgiysygiuzbcusucnc
+secret code = ydgiysygiuzbcusucncpassword =Ashwin@123
+
+                                 secret code = ydgiysygiuzbcusucnc
+
+                                 password =Ashwin@123
+
+                                 secret code = ydgiysygiuzbcusucnc
+
+
+
+pass@12356
+
