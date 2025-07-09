a6caccc3db518daebb94125189e0e3f7c515497e
Ashwin Vadde <ashwin.vadde@bilvantis.io>
2025-07-09 11:42:50 +0530

    lev

--- code.py
+++ code.py
@@ -1,35 +1,6 @@
 password =Ashwin@123
-
-secret code = ydgiysygiuzbcusucnc
-
-password =Ashwin@123
-
-secret code = ydgiysygiuzbcusucncpassword =Ashwin@123
-
-                                 secret code = ydgiysygiuzbcusucnc
-
-                                 password =Ashwin@123
-
-                                 secret code = ydgiysygiuzbcusucnc
-nndjfnjfj
-password =Ashwin@123
-
-secret code = ydgiysygiuzbcusucnc
-
-password =Ashwin@123
-
-secret code = ydgiysygiuzbcusucncpassword =Ashwin@123
-
-                                 secret code = ydgiysygiuzbcusucnc
-
-                                 password =Ashwin@123
-
-                                 secret code = ydgiysygiuzbcusucnc
-nndjfnjfj
-
-pass@12356
-
+ secret code = ydgiysygiuzbcusucnc
 API_KEY = "sk-test-123456"
-API_KEY = "sk-test-123456"
+
 
 
